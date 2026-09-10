419b879e72416f15803ffb23be7d5078776d6c98
Leon Chan <102279479+cyl2chan@users.noreply.github.com>
2026-09-10 10:32:09 -0400

    1) Add code to wait 2 seconds after each action to slow down the process for easier view during test, 2) Add code to remove invisible code of Byte Order Mark at the start of the first username in CSV file

--- tests/test_login_POM.py
+++ tests/test_login_POM.py
@@ -7,7 +7,7 @@ import pytest
 def get_csv_data() -> list:
     import csv
     data = []
-    with open("./test_data/data.csv", newline="") as csvfile:
+    with open("./test_data/data.csv", newline="", encoding="utf-8-sig") as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             data.append(row)
@@ -16,15 +16,19 @@ def get_csv_data() -> list:
 @pytest.mark.parametrize("username, password", get_csv_data())
 
 def test_login(page: Page, username, password) -> None:
+    print(repr(username))
+    print(repr(password))
+    
     login_page = LoginPage(page)
     products_page = ProductsPage(page)
     
     page.goto("http://www.saucedemo.com")
-    #login_page.enter_username("standard_user")
-    #login_page.enter_password("secret_sauce")
     login_page.enter_username(username)
+    page.wait_for_timeout(2000)
     login_page.enter_password(password)
+    page.wait_for_timeout(2000)
     login_page.click_login()
 
     products_page.display_swag_labs()
+    page.wait_for_timeout(2000)
 
