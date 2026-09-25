404b2dea1ae1eed886c0cda89e425bbd4430a342
Leon Chan <102279479+cyl2chan@users.noreply.github.com>
2026-09-25 16:39:40 -0400

    Purposefully enter wrong password to see what a failed report look like in Github Actions

--- pages/login_page.py
+++ pages/login_page.py
@@ -19,5 +19,5 @@ class LoginPage:
 
     def login(self):
         self.enter_username("standard_user")
-        self.enter_password("secret_sauce")
+        self.enter_password("secret_")
         self.click_login()
